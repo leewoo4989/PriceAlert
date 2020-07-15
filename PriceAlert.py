f7c83d26709bfcfb9e33f7f31775b578a7dbd25c
leewoo4989 <leewoo4989@gmail.com>
2020-07-15 01:01:52 -0400

    Update PriceAlert.py
    code improvements, remove redundancy and better follows the style guide

--- PriceAlert.py
+++ PriceAlert.py
@@ -21,24 +21,28 @@ def pushbullet_message(title, body):
         print("Message sent")
 
 
-def check_price():
+def grab_prices(urls):
+    grabbed_prices = []
+    for i in range(len(urls)):
+        browser.get(urls[i])
+        grabbed_prices.append(
+            float(re.search("[1-9]\d*(\.\d+)?", browser.find_element_by_xpath(xpaths[i]).text).group(0)))
+    return grabbed_prices
+
+
+def check_price(urls):
     while True:
         index = 0
-        for url in urls:
-            browser.get(url)
-            updated_price = float(
-                re.search("[1-9]\d*(\.\d+)?", browser.find_element_by_xpath(xpaths[index]).text).group(0))
-            if prices[index] != updated_price:
+        updated_prices = grab_prices(urls)
+        for i in range(len(updated_prices)):
+            if prices[index] != updated_prices[i]:
                 if config["Pushbullet"][enabled] == "true":
                     pushbullet_message("Listing price changed",
                                        "Price of listing {} changed from {} to ${}".format(url, prices[index],
                                                                                            updated_price))
                 logger.info("Price of listing {} changed from {} to ${}".format(url, prices[index], updated_price))
-        global delay
         sleep(delay)
 
-
-global delay
 
 driver_path = "C:/Program Files (x86)/chromedriver.exe"
 option = webdriver.ChromeOptions()
@@ -50,12 +54,13 @@ option.add_argument("disable-infobars")
 option.add_argument("--disable-extensions")
 
 browser = webdriver.Chrome(executable_path=driver_path, options=option)
+
 config = configparser.ConfigParser()
 config.read("config.ini")
+delay = int(config["Configuration"]["refresh_timer"])
 
 urls = []
 xpaths = []
-prices = []
 
 file = open("links.txt", "r")
 for line in file.readlines():
@@ -63,17 +68,10 @@ for line in file.readlines():
     urls.append(data[0])
     xpaths.append(data[1])
 
-index = 0
-for url in urls:
-    browser.get(url)
-    prices.append(float(
-        re.search("[1-9]\d*(\.\d+)?", browser.find_element_by_xpath(xpaths[index]).text).group(
-            0)))
-    logging.basicConfig(filename="pricehistory.log", level=logging.INFO)
-    logger = logging.getLogger()
-    index += 1
+prices = grab_prices(urls)
 
-delay = int(config["Configuration"]["refresh_timer"])
+logging.basicConfig(filename="pricehistory.log", level=logging.INFO)
+logger = logging.getLogger()
 
 price_thread = threading.Thread(target=checkPrice())
 price_thread.start()
